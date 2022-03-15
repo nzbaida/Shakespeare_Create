a775da4b87a7b4fa4619b828669709b3a28366fc
nzbaida <76817348+nzbaida@users.noreply.github.com>
2022-03-15 14:13:31 -0400

    Add files via upload

---
+++ Shakespeare_Insert.py
@@ -0,0 +1,41 @@
+import pymysql
+import time
+
+myConnection = pymysql.connect(host = 'localhost', user = 'admin', passwd = 'LocalSQL1123!', db = 'shakespeare')
+cur = myConnection.cursor()
+
+with open('characters.txt', 'r') as char:
+    charList = char.read().splitlines()
+curChar = 'Unknown'
+
+start_time = time.time()
+
+createSQL = 'INSERT INTO amnd (char_name, play_text) VALUES (%s,%s);'
+
+with open('A_Midsummer_Nights_Dream.txt', 'r') as playlines:
+    for line in playlines:
+        if line.upper().strip() in charList:
+            curChar = line.strip()
+            print('Changing character to ', curChar)
+        else:
+            sql_values = curChar, line.strip()
+            print('Writing line', line.strip())
+            cur.execute(createSQL, sql_values)
+
+myConnection.commit()
+end_time = time.time()
+
+cur.execute('SELECT COUNT(line_number) FROM amnd;')
+numPlayLines = cur.fetchall()[0][0]
+print(numPlayLines, ' rows')
+
+queryExecTime = end_time - start_time
+print('Total query time: ', queryExecTime)
+queryTimePerLine = queryExecTime / numPlayLines
+print('Query time per line ', queryTimePerLine)
+
+insertPerfSQL = 'INSERT INTO performance VALUES ("CREATE", %s);'
+cur.execute(insertPerfSQL, queryTimePerLine)
+
+myConnection.commit()
+myConnection.close()
